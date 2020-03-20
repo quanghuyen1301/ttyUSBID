8272ab49250a07015490909e5b6eae150dda908e
hqbui <hqbui@amperecomputing.com>
2020-03-20 09:37:11 +0700

    Update

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # ttyUSBID
-list|short|conversion ttyUSB serial id
+List | Sort | Converter ttyUSB id to serial number
+## Install
+	$git clone https://github.com/quanghuyen1301/ttyUSBID.git
+	$cd ttyUSBID 
+	$pip install .
+## List ttyUSB id and serial number Sort by kernel mount ttyUSB
+	$python -m ttyUSBID
+	TIME TTYUSB SERIAL
+	[2644609.259334] ttyUSB5 FTHK9JP9
+	[2644606.689701] ttyUSB4 FTBELQCK
+	[2467077.574854] ttyUSB8 A10179GG
+	[2467002.839031] ttyUSB9 AC01X49W
+	[1865270.430936] ttyUSB7 AM00F4EM
+	[1805605.119032] ttyUSB1 A6018654
+	[1365036.902674] ttyUSB3 FTAWCC3C
+	[1365022.519438] ttyUSB2 FTAXN4DG
+	[1365016.055157] ttyUSB0 FTAXMM1E
+## Converter ttyUSB id to serial number
+	$python -m ttyUSBID ttyUSB0
+	FTAXMM1E
+## Converter ttyUSB serial number to id
+	$python -m ttyUSBID FTAXMM1E
+	ttyUSB0
+## Open ttyUSB via serial number
+	$minicom -D /dev/``python -m ttyUSBID FTAXMM1E``
--- ttyUSBID/__main__.py
+++ ttyUSBID/__main__.py
@@ -1,7 +1,51 @@
+import sys
 import commands
-if __name__ == "__main__":
-    print "Hello"
-    re=commands.getstatusoutput("for sysdevpath in $(find /sys/bus/usb/devices/usb*/ -name dev | grep ttyUSB);\
-         do udevadm info -q property --export -p `dirname $sysdevpath` |\
+def find_in( s, first, last):
+    try:
+        start = s.index( first ) + len( first ) 
+        if last =="":
+            end = len(s)
+        else:
+            end = s.index( last, start ) 
+        return s[start:end]
+    except ValueError:
+        return ""
+def tty2id(tty_serials,ttyUSB):
+    if ttyUSB.find('tty') != -1:
+        for tty_serial in tty_serials:
+            if tty_serial.find(ttyUSB) != -1:
+                re= find_in(tty_serial,"ID_SERIAL_SHORT='","'")
+                if re != "" :return re
+    return ttyUSB
+def id2tty(tty_serials,ttyUSB):
+    if ttyUSB.find('tty') == -1:
+        for tty_serial in tty_serials:
+            if tty_serial.find(ttyUSB) != -1:
+                re= find_in(tty_serial,"/dev/","'")
+                if re != "" :return re
+    return ttyUSB
+def get_ttydata():
+    tty_serial=commands.getstatusoutput("for i in $(find /sys/bus/usb/devices/usb*/ -name dev | grep ttyUSB);\
+         do udevadm info -q property --export -p `dirname $i` |\
          grep --color=never  'DEVNAME\|ID_SERIAL_SHORT';done")
-    print re
+    tty_dmesg=commands.getstatusoutput("dmesg | grep 'to ttyUSB'")
+    return tty_serial[1].split("DEVNAME="),tty_dmesg[1].split("\n")
+if __name__ == "__main__":
+    tty_serials,tty_dmesgs = get_ttydata()
+    if len(sys.argv) == 1 :
+        print "TIME TTYUSB SERIAL"
+        ttyUSBID=[]
+        for tty_dmesg in reversed(tty_dmesgs):
+            ttyUSB=find_in(tty_dmesg," to ","")
+            if (ttyUSB !="") and (ttyUSB.find('tty') != -1) and ((ttyUSB in ttyUSBID) == False):
+                id = tty2id(tty_serials,ttyUSB)
+                if id != ttyUSB:
+                    ttyUSBID.append(ttyUSB)
+                    print "[%s] %s %s" %(find_in(tty_dmesg,"[","]"),ttyUSB,id)
+    if len(sys.argv) >= 2 :
+        ttyUSB=sys.argv[1]
+        if sys.argv[1].find('tty') == -1:
+            re = id2tty(tty_serials,ttyUSB)
+        else:
+            re = tty2id(tty_serials,ttyUSB)
+        print re
